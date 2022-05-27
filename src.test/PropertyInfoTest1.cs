d0963afe517cca2fe1ea5ba8006b4477503a1ab9
m.samson <m.samson@opencodedev.com>
2022-05-26 21:11:34 -0600

    Added new feature methods

--- src.test/PropertyInfoTest1.cs
+++ src.test/PropertyInfoTest1.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace OpenCodeDev.Reflection.Helper.Test
 {
+    public class MyObjTester
+    {
+
+    }
+
     public class MyTypeTester
     {
         public bool BoolType { get; set; }
+
+        [EmailAddress]
         public string StringType { get; set; }
+
         public long LongType { get; set; }
         public static double DoubleType { get; set; }
         private static float FloatType { get; set; }
@@ -11,74 +22,229 @@ namespace OpenCodeDev.Reflection.Helper.Test
         public decimal DecimalType { get; set; }
         public object ObjectType { get; set; }
         public DateTime TimeOfTheYear { get; set; }
+        public Object BigObject { get; set; } = null;
+        public String BigString { get; set; } 
+
+        public MyObjTester SubObj { get; set; }
+
+        public List<object> MyListObjects { get; set; } = new List<object>();
+        public List<string> MyListString { get; set; } = new List<string>();
+        public List<String> MyListBigString { get; set; } = new List<String>();
     }
     [TestClass]
     public class PropertyInfoTest1
     {
-        [TestMethod("PropertyInfo Test Correct Types")]
-        public void Test1()
+        Type myObj = typeof(MyTypeTester);
+
+        [TestMethod("PropertyInfo::IsList()")]
+        public void IsListTests()
         {
-            MyTypeTester myObj = new MyTypeTester();
-            var runtimeObj = myObj.GetType();
-
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("BoolType")?.IsBool());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("StringType")?.IsString());
-            Assert.IsTrue(runtimeObj?.GetPropertyUnrest("LongType")?.IsLong());
-
-            Assert.IsTrue(runtimeObj?.GetPropertyStaticPublic("DoubleType")?.IsDouble());
-            Assert.IsTrue(runtimeObj?.GetPropertyStatic("DoubleType")?.IsDouble());
-
-            Assert.IsTrue(runtimeObj?.GetPropertyStaticPrivate("FloatType")?.IsFloat());
-            Assert.IsTrue(runtimeObj?.GetPropertyStatic("FloatType")?.IsFloat());
-
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("ShortType")?.IsShort());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("DecimalType")?.IsDecimal());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("ObjectType")?.IsObject());
-
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("TimeOfTheYear")?.IsDateTime());
-
+            PropertyInfo? bList = myObj?.GetPropertyPublic("MyListBigString");
+            PropertyInfo? bList2 = myObj?.GetPropertyPublic("MyListString");
+            // Pos Test
+            Assert.IsTrue(bList?.IsList());
+            Assert.IsTrue(bList2?.IsList(true));
         }
 
-        [TestMethod("PropertyInfo Test Mix Types")]
-        public void Test2()
+        [TestMethod("PropertyInfo::IsObject() [Generic:object]")]
+        public void IsGenericObjectTests()
         {
-            MyTypeTester myObj = new MyTypeTester();
-            var runtimeObj = myObj.GetType();
-
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("BoolType")?.IsInt());
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("StringType")?.IsBool());
-            Assert.IsFalse(runtimeObj?.GetPropertyUnrest("LongType")?.Is<Int16>());
-
-            Assert.IsFalse(runtimeObj?.GetPropertyStaticPublic("DoubleType")?.IsFloat());
-
-            Assert.IsFalse(runtimeObj?.GetPropertyStaticPrivate("FloatType")?.IsDecimal());
-
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("ShortType")?.IsInt());
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("DecimalType")?.IsFloat());
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("ObjectType")?.IsBool());
-
-            Assert.IsFalse(runtimeObj?.GetPropertyPublic("TimeOfTheYear")?.IsDecimal());
-
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("ObjectType")?.IsObject());
+            Assert.IsTrue(myObj?.GetPropertyPublic("ObjectType")?.IsObject(true));
+            Assert.IsTrue(myObj?.GetPropertyPublic("ObjectType")?.Is<Object>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ObjectType")?.IsString());
         }
 
-        [TestMethod("PropertyInfo Test Struct Types")]
-        public void Test3()
+        [TestMethod("PropertyInfo::IsObject() [Generic:Object]")]
+        public void IsObjectTests()
         {
-            MyTypeTester myObj = new MyTypeTester();
-            var runtimeObj = myObj.GetType();
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("BigObject")?.IsObject());
+            Assert.IsTrue(myObj?.GetPropertyPublic("BigObject")?.IsObject(true));
+            Assert.IsTrue(myObj?.GetPropertyPublic("BigObject")?.Is<object>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigObject")?.IsString());
+        }
 
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("BoolType")?.Is<Boolean>());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("StringType")?.Is<String>());
-            Assert.IsTrue(runtimeObj?.GetPropertyUnrest("LongType")?.Is<Int64>());
+        [TestMethod("PropertyInfo::IsObject() [Generic:Class]")]
+        public void IsClassObjectTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("SubObj")?.IsObject());
+            Assert.IsTrue(myObj?.GetPropertyPublic("SubObj")?.IsObject(true));
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.Is<object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.Is<Object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyPublic("SubObj")?.IsString());
+        }
 
-            Assert.IsTrue(runtimeObj?.GetPropertyStaticPublic("DoubleType")?.Is<Double>());
+        [TestMethod("PropertyInfo::IsObject() [Generic:String]")]
+        public void IsStringObjectTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("BigString")?.IsObject());
 
-            Assert.IsTrue(runtimeObj?.GetPropertyStaticPrivate("FloatType")?.Is<Single>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.Is<object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.Is<Object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BigString")?.IsShort());
+        }
 
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("ShortType")?.Is<Int16>());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("DecimalType")?.Is<Decimal>());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("ObjectType")?.Is<Object>());
-            Assert.IsTrue(runtimeObj?.GetPropertyPublic("TimeOfTheYear")?.Is<DateTime>());
+        [TestMethod("PropertyInfo::IsString()")]
+        public void IsStringTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("StringType")?.IsObject());
+            Assert.IsTrue(myObj?.GetPropertyPublic("StringType")?.IsString());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.Is<object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.Is<Object>());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("StringType")?.IsShort());
+        }
+
+        [TestMethod("PropertyInfo::IsDecimal()")]
+        public void IsDecimalTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("DecimalType")?.IsDecimal());
+            Assert.IsTrue(myObj?.GetPropertyPublic("DecimalType")?.Is<decimal>());
+            Assert.IsTrue(myObj?.GetPropertyPublic("DecimalType")?.Is<Decimal>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("DecimalType")?.IsArray());
+        }
+
+        [TestMethod("PropertyInfo::IsShort()")]
+        public void IsShortTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("ShortType")?.IsShort());
+            Assert.IsTrue(myObj?.GetPropertyPublic("ShortType")?.Is<short>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("ShortType")?.IsArray());
+        }
+
+
+        [TestMethod("PropertyInfo::IsBool()")]
+        public void IsBoolTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("BoolType")?.IsBool());
+            Assert.IsTrue(myObj?.GetPropertyPublic("BoolType")?.Is<Boolean>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("BoolType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BoolType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyPublic("BoolType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("BoolType")?.IsArray());
+        }
+
+        [TestMethod("PropertyInfo::IsLong()")]
+        public void IsLongTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyPublic("LongType")?.IsLong());
+            Assert.IsTrue(myObj?.GetPropertyPublic("LongType")?.Is<long>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyPublic("LongType")?.IsArray());
+        }
+
+        [TestMethod("PropertyInfo::IsFloat()")]
+        public void IsFloatTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyStatic("FloatType")?.IsFloat());
+            Assert.IsTrue(myObj?.GetPropertyStatic("FloatType")?.Is<float>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsDouble());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyStatic("FloatType")?.IsArray());
+        }
+
+        [TestMethod("PropertyInfo::IsDouble()")]
+        public void IsDoubleTests()
+        {
+            // Pos Test
+            Assert.IsTrue(myObj?.GetPropertyStaticPublic("DoubleType")?.IsDouble());
+            Assert.IsTrue(myObj?.GetPropertyStaticPublic("DoubleType")?.Is<Double>());
+            Assert.IsTrue(myObj?.GetPropertyStaticPublic("DoubleType")?.Is<double>());
+            // Negative Test
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsInt());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsShort());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsFloat());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsLong());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsDecimal());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsObject());
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsObject(true));
+            Assert.IsFalse(myObj?.GetPropertyStaticPublic("DoubleType")?.IsArray());
+        }
+
+
+
+        [TestMethod("PropertyInfo::HasAttribute<>()")]
+        public void HasAttributeTests()
+        {
+            PropertyInfo? prop = myObj?.GetPropertyPublic("StringType");
+            Assert.IsTrue(prop?.HasAttribute<EmailAddressAttribute>());
         }
     }
+
+
+    
 }
--- src/PropertyInfoExt.cs
+++ src/PropertyInfoExt.cs
@@ -1,19 +1,79 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
 
 namespace OpenCodeDev.Reflection.Helper
 {
     public static class PropertyInfoExt
     {
         public static bool IsBool(this PropertyInfo prop) { return prop.PropertyType == typeof(bool); }
-        public static bool IsString(this PropertyInfo prop) { return prop.PropertyType == typeof(string); }
+        public static bool IsString(this PropertyInfo prop) { return prop.PropertyType == typeof(String); }
         public static bool IsFloat(this PropertyInfo prop) { return prop.PropertyType == typeof(float); }
         public static bool IsDecimal(this PropertyInfo prop) { return prop.PropertyType == typeof(decimal); }
         public static bool IsDouble(this PropertyInfo prop) { return prop.PropertyType == typeof(double); }
         public static bool IsInt(this PropertyInfo prop) { return prop.PropertyType == typeof(int); }
         public static bool IsLong(this PropertyInfo prop) { return prop.PropertyType == typeof(long); }
         public static bool IsShort(this PropertyInfo prop) { return prop.PropertyType == typeof(short); }
-        public static bool IsObject(this PropertyInfo prop) { return prop.PropertyType == typeof(object); }
         public static bool IsDateTime(this PropertyInfo prop) { return prop.PropertyType == typeof(DateTime); }
-        public static bool Is<T>(this PropertyInfo prop) { return prop.PropertyType == typeof(T); }
+
+
+        /// <summary>
+        /// Check if type is Class Object
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <param name="ignoreGeneric">True: will not System.Type and Generic Types like List, Dictionary</param>
+        /// <returns></returns>
+        public static bool IsObject(this PropertyInfo prop, bool ignoreGeneric = false) { 
+            return !ignoreGeneric ?
+                prop.PropertyType.IsClass :
+                prop.PropertyType.IsClass && !prop.PropertyType.IsPrimitive && 
+                !prop.PropertyType.IsGenericType && !prop.IsString(); 
+        }
+
+        public static bool IsArray(this PropertyInfo prop) { return prop.PropertyType.IsArray; }
+
+
+
+        /// <summary>
+        /// Check if prop is a List
+        /// </summary>
+        /// <param name="ignoreGeneric">True: Ignore List of List, List of int so on.... only check for List of ClassObject</param>
+        public static bool IsList(this PropertyInfo prop, bool ignoreGeneric = false)
+        {
+            return !ignoreGeneric ? 
+                prop.PropertyType.IsClass && prop.IsGeneric(typeof(List<>)) :
+                (prop.IsList() && !prop.GetListType().IsPrimitive && !prop.GetListType().IsGenericType && !prop.IsString());
+        }
+
+        public static bool IsListType<T>(this PropertyInfo prop)
+        {
+            return prop.GetListType() == typeof(T);
+        }
+
+        /// <summary>
+        /// Return type argument in list List of Type
+        /// </summary>
+        public static Type? GetListType(this PropertyInfo prop)
+        { return !prop.IsList() ? null : prop.PropertyType.GetGenericArguments()[0]; }
+
+
+        public static bool Is<T>(this PropertyInfo prop) { return prop.Is(typeof(T)); }
+
+
+
+        /// <summary>
+        /// Check if prop is generic to X used to check list prop.IsGeneric(typeof(List<>))
+        /// </summary>
+        public static bool IsGeneric(this PropertyInfo prop, Type type) { 
+            return 
+                prop.PropertyType.IsGenericType && 
+                prop.PropertyType.GetGenericTypeDefinition() == type; 
+        }
+
+        public static bool Is(this PropertyInfo prop, Type type) { return prop.PropertyType == type; }
+
+        /// <summary>
+        /// Check if an attribute exist on a property return false if prop null and if not found or if attribute == null
+        /// </summary>
+        public static bool HasAttribute<T>(this PropertyInfo prop) { return prop == null ? false : prop.GetCustomAttribute(typeof(T)) != null; }
     }
 }
